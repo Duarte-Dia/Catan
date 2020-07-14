a61f53d55f345861e9feb76ffe325c02320103c4
Duarte-Dias <61877146+Duarte-Dia@users.noreply.github.com>
2020-07-14 19:56:02 +0100

    grande transferencia de metodos da main para a classe servidor

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,9 +5,23 @@
  */
 package Server;
 
+import catan.Board;
+import catan.City;
+import catan.Dice;
+import catan.FXMLDocumentController;
+import catan.Hexagon;
+import catan.Main;
+import catan.Player;
+import catan.Settlement;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -18,34 +32,64 @@ public class Server {
     private static int port = 6666, nClientes = 1;
     private static Vector<ClientHandler> listaClientes = new Vector<>();
     private static Socket client;
-    
+    public static TextArea chat;
+    private static TextField inputChat;
+    static int idJogadorLocal = 1, i;
+    static boolean gameover, endPlay;
+    static Dice dice = new Dice();
+    static List<Player> listPlayers = new ArrayList<Player>();
+    public static Tab tp1, tp2, tp3, tp4;
+    public static Button endTurn, roadButton;
+    static DataInputStream in;
+    static DataOutputStream out;
+    static int chosenTile = 0;
+    static Board board = new Board();
 
     public static void main(String[] args) throws IOException {
-
-        ServerSocket server = new ServerSocket(port);
         
-
+        
+        
+        
+        ServerSocket server = new ServerSocket(port);
+        Thread servidor = new Thread(() -> {
         while (true) {
             // Servidor fica a espera de um cliente
-            if(nClientes <=4 ){
-            System.out.println("[SERVER]Esperando por ligação");
-             client = server.accept();
-            System.out.println("[SERVER]Estabelecido ligação a um cliente");
-            
-            DataInputStream in = new DataInputStream(client.getInputStream());
-            DataOutputStream out = new DataOutputStream(client.getOutputStream());
-
-            ClientHandler ch = new ClientHandler("Cliente " + nClientes, in, out, client);
-            Thread t = new Thread(ch);
-
-            System.out.println("[SERVER]Cliente " + nClientes + " adicionado ");
-            listaClientes.add(ch);
-
-            out.writeUTF("Sucesso a Conectar o Cliente "+nClientes+" \n");
-            out.writeUTF("#SetPlayer"+nClientes);
-            t.start();
-
-            nClientes++;}
+            if (nClientes <= 4) {
+                try {
+                    System.out.println("[SERVER]Esperando por ligação");
+                    try {
+                        client = server.accept();
+                    } catch (IOException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    System.out.println("[SERVER]Estabelecido ligação a um cliente");
+                    
+                    in = new DataInputStream(client.getInputStream());
+                    out = new DataOutputStream(client.getOutputStream());
+                    
+                    ClientHandler ch = new ClientHandler("Cliente " + nClientes, in, out, client);
+                    Thread t = new Thread(ch);
+                    
+                    System.out.println("[SERVER]Cliente " + nClientes + " adicionado ");
+                    listaClientes.add(ch);
+                    
+                    try {
+                        out.writeUTF("Sucesso a Conectar o Cliente " + nClientes + " \n");
+                    } catch (IOException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    try {
+                        out.writeUTF("#SetPlayer" + nClientes);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    t.start();
+                    
+                    nClientes++;
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
 
         }
 
@@ -54,6 +98,132 @@ public class Server {
          client.close();
          System.out.println("[Server]A desligar");
          server.close();*/
+        });
+        
+        servidor.start();
+
+        chat = FXMLDocumentController.chat;
+        inputChat = FXMLDocumentController.inputChat;
+
+        tp1 = FXMLDocumentController.tp1;
+        tp2 = FXMLDocumentController.tp2;
+        tp3 = FXMLDocumentController.tp3;
+        tp4 = FXMLDocumentController.tp4;
+        endTurn = FXMLDocumentController.endTurn;
+        roadButton = FXMLDocumentController.roadBtn;
+
+        Player p1 = new Player(0, 1, 0, 0, 0, 0, 0, false, false);
+        Player p2 = new Player(0, 2, 0, 0, 0, 0, 0, false, false);
+        Player p3 = new Player(0, 3, 0, 0, 0, 0, 0, false, false);
+        listPlayers.add(p1);
+        listPlayers.add(p2);
+        listPlayers.add(p3);
+
+        
+        
+        
+        
+        
+        
+        
+        gameover = false;
+        
+        Thread jogo;
+        jogo = new Thread(() -> {
+            
+            while (!gameover) {
+
+                for (i = 1; i <= listPlayers.size();) {
+                    endPlay = false;
+                    // possivel problema
+                    chosenTile = dice.throwDice(2);
+
+                    if (chosenTile != 7) {
+                        for (Hexagon hex : board.getTiles()) {
+                            if (chosenTile == hex.getNum()) {
+                                givePlayersResources(hex.getResourceID(), hex);
+                            }
+                        }
+                    } else {
+                        // LANÇOU 7
+                        // MOVE LADRAO
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    try{
+                        roadButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                            @Override
+                            public void handle(MouseEvent m) {
+                                for (Node n : FXMLDocumentController.linesGroup.getChildren()) {
+                                    if (idJogadorLocal == i) {
+                                        System.out.println(n.getId() + "\n");
+                                        n.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                                            @Override
+                                            public void handle(MouseEvent m) {
+                                                if (idJogadorLocal == i) {
+                                                    System.out.println(n.getId());
+                                                    // player tem recursos?
+                                                    // rua disponivel?
+                                                    // 
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    } catch(Exception e ){
+                   
+                    }
+                    
+                    
+                    
+                    
+                    try{
+                    endTurn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent m) {
+                            if (idJogadorLocal == i) {
+                                System.out.println("Next player");
+                                endPlay = true;
+                                try {
+                                    out.writeUTF("end Turn button pressed");
+                                } catch (IOException ex) {
+                                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }
+                        }
+                    });
+                    } catch(Exception e ){
+                    
+                    e = new NullPointerException();
+                    }
+
+                    if (endPlay) {
+                        i++;
+                    }
+
+                    longestRoad();
+                    biggestArmy();
+
+                    if (isGameOver()) {
+                        gameover = true;
+                    }
+                }
+            }
+
+        });
+        
+
+        jogo.start();
+
+        
+
+        
     }
 
     private static class ClientHandler implements Runnable {
@@ -102,7 +272,6 @@ public class Server {
                             if (client.name.equals(receivingClient) && client.logged) {
                                 client.out.writeUTF("Whisper from " + name + ": " + msg);
                             }
-
                         }
                     } else {
 
@@ -122,4 +291,139 @@ public class Server {
         }
 
     }
+    
+    
+       private static void givePlayersResources(int resource, Hexagon hex) {
+        int current;
+        for (Player p : listPlayers) {
+            for (City c : p.getListCities()) {
+                if (hex.containVector(c.getPosition())) {
+                    switch (resource) {
+                        case 1: // wool
+                            current = p.getWool();
+                            p.setWool(current += 2);
+                            break;
+                        case 2: // timber
+                            current = p.getTimber();
+                            p.setTimber(current += 2);
+                            break;
+                        case 3: // brick
+                            current = p.getBrick();
+                            p.setBrick(current += 2);
+                            break;
+                        case 4: // wheat
+                            current = p.getWheat();
+                            p.setWheat(current += 2);
+                            break;
+                        case 5: // metal
+                            current = p.getMetal();
+                            p.setMetal(current += 2);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            for (Settlement s : p.getListSettlements()) {
+                if (hex.containVector(s.getPosition())) {
+                    switch (resource) {
+                        case 1: // wool
+                            current = p.getWool();
+                            p.setWool(current++);
+                            break;
+                        case 2: // timber
+                            current = p.getTimber();
+                            p.setTimber(current++);
+                            break;
+                        case 3: // brick
+                            current = p.getBrick();
+                            p.setBrick(current++);
+                            break;
+                        case 4: // wheat
+                            current = p.getWheat();
+                            p.setWheat(current++);
+                            break;
+                        case 5: // metal
+                            current = p.getMetal();
+                            p.setMetal(current++);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean isGameOver() {
+
+        for (Player p : listPlayers) {
+            p.addScore(p.getListSettlements().size());
+
+            p.addScore(p.getListCities().size() * 2);
+
+            if (p.isLongestRoad()) {
+                p.addScore(2);
+            }
+
+            if (p.isBiggestArmy()) {
+                p.addScore(2);
+            }
+
+            //p.addScore(p.devCardsPoints());
+            if (p.getScore() >= 10) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void longestRoad() {
+        List<Integer> listRoadSizes = new ArrayList<Integer>();
+        int size, playerSelected;
+
+        for (Player p : listPlayers) {
+            listRoadSizes.add(p.getListRoads().size());
+        }
+
+        size = Collections.max(listRoadSizes);
+
+        if (size >= 5) {
+            if (Collections.frequency(listRoadSizes, size) == 1) {
+                playerSelected = listRoadSizes.indexOf(Collections.max(listRoadSizes));
+
+                for (Player p : listPlayers) {
+                    p.setLongestRoad(false);
+                }
+
+                listPlayers.get(playerSelected).setLongestRoad(true);
+            }
+        }
+    }
+
+    private static void biggestArmy() {
+        List<Integer> listArmySizes = new ArrayList<Integer>();
+        int size, playerSelected;
+
+        for (Player p : listPlayers) {
+            listArmySizes.add(p.getArmy());
+        }
+
+        size = Collections.max(listArmySizes);
+
+        if (size >= 3) {
+            if (Collections.frequency(listArmySizes, size) == 1) {
+                playerSelected = listArmySizes.indexOf(Collections.max(listArmySizes));
+
+                for (Player p : listPlayers) {
+                    p.setBiggestArmy(false);
+                }
+
+                listPlayers.get(playerSelected).setBiggestArmy(true);
+            }
+        }
+    }
+
 }
--- src/catan/Main.java
+++ src/catan/Main.java
@@ -5,6 +5,11 @@
  */
 package catan;
 
+import static Server.Server.chat;
+import static Server.Server.tp1;
+import static Server.Server.tp2;
+import static Server.Server.tp3;
+import static Server.Server.tp4;
 import common.Le;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -33,23 +38,17 @@ public class Main extends Application {
 
     private static String serverIP = "127.0.0.1";
     private static final int serverPort = 6666;
-
-    static int chosenTile = 0;
-    int idJogadorLocal = 1, i;
-    static boolean gameover, endPlay;
-    static Dice dice = new Dice();
-    static List<Player> listPlayers = new ArrayList<Player>();
-    static Board board = new Board();
     public static TextArea chat;
     private static TextField inputChat;
     public static Tab tp1, tp2, tp3, tp4;
-    public static Button endTurn, roadButton;
-        DataInputStream in ;
-        DataOutputStream out;
+    static DataInputStream in;
+    static DataOutputStream out;
+    int idJogadorLocal = 1, i;
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
         chat = FXMLDocumentController.chat;
         inputChat = FXMLDocumentController.inputChat;
 
@@ -57,8 +56,7 @@ public class Main extends Application {
         tp2 = FXMLDocumentController.tp2;
         tp3 = FXMLDocumentController.tp3;
         tp4 = FXMLDocumentController.tp4;
-        endTurn = FXMLDocumentController.endTurn;
-        roadButton = FXMLDocumentController.roadBtn;
+        
 
         Scene scene = new Scene(root);
 
@@ -66,227 +64,12 @@ public class Main extends Application {
         stage.show();
 
         connectClient();
-        Player p1 = new Player(0, 1, 0, 0, 0, 0, 0, false, false);
-        Player p2 = new Player(0, 2, 0, 0, 0, 0, 0, false, false);
-        Player p3 = new Player(0, 3, 0, 0, 0, 0, 0, false, false);
-        listPlayers.add(p1);
-        listPlayers.add(p2);
-        listPlayers.add(p3);
-
-        Thread jogo = new Thread(() -> {
-
-            while (!gameover) {
-
-                for (i = 1; i <= listPlayers.size();) {
-                    endPlay = false;
-                    // possivel problema
-                    chosenTile = dice.throwDice(2);
-
-                    if (chosenTile != 7) {
-                        for (Hexagon hex : board.getTiles()) {
-                            if (chosenTile == hex.getNum()) {
-                                givePlayersResources(hex.getResourceID(), hex);
-                            }
-                        }
-                    } else {
-                        // LANÇOU 7
-                        // MOVE LADRAO
-                    }
-
-                    roadButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                        @Override
-                        public void handle(MouseEvent m) {
-                            for (Node n : FXMLDocumentController.linesGroup.getChildren()) {
-                                if (idJogadorLocal == i) {
-                                    System.out.println(n.getId() + "\n");
-                                    n.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                                        @Override
-                                        public void handle(MouseEvent m) {
-                                            if (idJogadorLocal == i) {
-                                                System.out.println(n.getId());
-                                                // player tem recursos?
-                                                // rua disponivel?
-                                                // 
-                                            }
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    });
-
-                    endTurn.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                        @Override
-                        public void handle(MouseEvent m) {
-                            if (idJogadorLocal == i) {
-                                System.out.println("Next player");
-                                endPlay = true;
-                                try {
-                                    out.writeUTF("end Turn button pressed");
-                                } catch (IOException ex) {
-                                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-                            }
-                        }
-                    });
-
-                    if (endPlay) {
-                        i++;
-                    }
-
-                    longestRoad();
-                    biggestArmy();
-
-                    if (isGameOver()) {
-                        gameover = true;
-                    }
-                }
-            }
-
-        });
-
-        jogo.start();
-
-        gameover = false;
 
     }
 
     public static void main(String[] args) throws UnknownHostException, IOException {
 
         launch(args);
-    }
-
-    private static void givePlayersResources(int resource, Hexagon hex) {
-        int current;
-        for (Player p : listPlayers) {
-            for (City c : p.getListCities()) {
-                if (hex.containVector(c.getPosition())) {
-                    switch (resource) {
-                        case 1: // wool
-                            current = p.getWool();
-                            p.setWool(current += 2);
-                            break;
-                        case 2: // timber
-                            current = p.getTimber();
-                            p.setTimber(current += 2);
-                            break;
-                        case 3: // brick
-                            current = p.getBrick();
-                            p.setBrick(current += 2);
-                            break;
-                        case 4: // wheat
-                            current = p.getWheat();
-                            p.setWheat(current += 2);
-                            break;
-                        case 5: // metal
-                            current = p.getMetal();
-                            p.setMetal(current += 2);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
-            for (Settlement s : p.getListSettlements()) {
-                if (hex.containVector(s.getPosition())) {
-                    switch (resource) {
-                        case 1: // wool
-                            current = p.getWool();
-                            p.setWool(current++);
-                            break;
-                        case 2: // timber
-                            current = p.getTimber();
-                            p.setTimber(current++);
-                            break;
-                        case 3: // brick
-                            current = p.getBrick();
-                            p.setBrick(current++);
-                            break;
-                        case 4: // wheat
-                            current = p.getWheat();
-                            p.setWheat(current++);
-                            break;
-                        case 5: // metal
-                            current = p.getMetal();
-                            p.setMetal(current++);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        }
-    }
-
-    private static boolean isGameOver() {
-
-        for (Player p : listPlayers) {
-            p.addScore(p.getListSettlements().size());
-
-            p.addScore(p.getListCities().size() * 2);
-
-            if (p.isLongestRoad()) {
-                p.addScore(2);
-            }
-
-            if (p.isBiggestArmy()) {
-                p.addScore(2);
-            }
-
-            //p.addScore(p.devCardsPoints());
-            if (p.getScore() >= 10) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static void longestRoad() {
-        List<Integer> listRoadSizes = new ArrayList<Integer>();
-        int size, playerSelected;
-
-        for (Player p : listPlayers) {
-            listRoadSizes.add(p.getListRoads().size());
-        }
-
-        size = Collections.max(listRoadSizes);
-
-        if (size >= 5) {
-            if (Collections.frequency(listRoadSizes, size) == 1) {
-                playerSelected = listRoadSizes.indexOf(Collections.max(listRoadSizes));
-
-                for (Player p : listPlayers) {
-                    p.setLongestRoad(false);
-                }
-
-                listPlayers.get(playerSelected).setLongestRoad(true);
-            }
-        }
-    }
-
-    private static void biggestArmy() {
-        List<Integer> listArmySizes = new ArrayList<Integer>();
-        int size, playerSelected;
-
-        for (Player p : listPlayers) {
-            listArmySizes.add(p.getArmy());
-        }
-
-        size = Collections.max(listArmySizes);
-
-        if (size >= 3) {
-            if (Collections.frequency(listArmySizes, size) == 1) {
-                playerSelected = listArmySizes.indexOf(Collections.max(listArmySizes));
-
-                for (Player p : listPlayers) {
-                    p.setBiggestArmy(false);
-                }
-
-                listPlayers.get(playerSelected).setBiggestArmy(true);
-            }
-        }
     }
 
     private void connectClient() throws IOException {
