555856e53698cb9c9aebabbabf54d2e1d0ee80a7
Bruno <brunoribeiro.15@hotmail.com>
2020-07-16 04:38:12 +0100

    board hexagons work

--- src/catan/Board.java
+++ src/catan/Board.java
@@ -5,7 +5,11 @@
  */
 package catan;
 
+import java.io.*;
+import java.net.URL;
 import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 /**
  *
@@ -14,6 +18,7 @@ import java.util.*;
 public class Board {
 
     private List<Hexagon> tiles = new ArrayList<Hexagon>();
+    private Object parser;
 
     public Board() {
         //this.tiles = tiles;
@@ -77,6 +82,8 @@ public class Board {
 
         Hexagon newHex19 = new Hexagon(18, 6, 2);
         tiles.add(newHex19);
+
+        defineHexagonVectors();
     }
 
     public List<Hexagon> getTiles() {
@@ -85,6 +92,36 @@ public class Board {
 
     public void setTiles(List<Hexagon> tiles) {
         this.tiles = tiles;
+    }
+
+    public void defineHexagonVectors() {
+        JSONParser parser = new JSONParser();
+
+        try {
+            String p = Board.class.getResource("Hexagons.json").getPath();
+            p = p.substring(6, p.length() - 35).concat("src/catan/Hexagons.json");
+            System.out.println(p);
+            Object obj = parser.parse(new FileReader(p));
+
+            JSONObject jsonObj = (JSONObject) obj;
+            JSONObject hexs = (JSONObject) jsonObj.get("hexagons");
+
+            for (int i = 1; i < 20; i++) {
+                JSONArray array = (JSONArray) hexs.get(Integer.toString(i));
+                for (int j = 0; j < 6; j++) {
+                    JSONArray vectors = (JSONArray) array.get(j);
+                    tiles.get(i - 1).addVectors(new Vector3(Integer.parseInt(vectors.get(0).toString()),
+                            Integer.parseInt(vectors.get(1).toString()),
+                            Integer.parseInt(vectors.get(2).toString())));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
